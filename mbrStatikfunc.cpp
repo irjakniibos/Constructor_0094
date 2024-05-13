cad06125da76a5ab9c645a113b4b6c3cc8e19131
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-05-13 15:06:13 +0700

    membuat member

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -2,7 +2,23 @@
 #include <string>
 using namespace std;
 
-class mahasiswa 
+class Mahasiswa 
 {
+private:
+    static int nim;
 
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printALL();
+
+    static void setNim(int pNim) {nim = pNim;}
+    static int getNim() {return nim;}
+
+    Mahasiswa(string pnama):nama(pnama)
+    {
+        setID();
+    }
 };
