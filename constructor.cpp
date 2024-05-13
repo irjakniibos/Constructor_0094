395880ef417a2abd622d4dff7e67fa8ecb509866
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-05-13 15:53:27 +0700

    membuat program constructor

---
+++ constructor.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+public :
+    int nim;
+    string nama;
+    //constructor 1
+    Mahasiswa()
+    {
+        nim =0;
+        nama="";
+    };
+    //constructor 2
+    Mahasiswa( int iNim)
+    {
+        nim =iNim;
+    };
+    //constructor 3
+    Mahasiswa(string);
+
+    //constructor 4
+    Mahasiswa(int iNim,string iNama);
+
+
+    void cetak(){
+        cout << "NIM = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+        cout << endl;
+    }
+};
+
+Mahasiswa :: Mahasiswa (string iNama){
+    nama = iNama;
+}
+
+Mahasiswa :: Mahasiswa(int iNim,string iNama)
+    {
+    nim = iNim;
+    nama =iNama;
+    };
+
+int main (){
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(94);
+    Mahasiswa mhs3("Irza");
+    Mahasiswa mhs4(94,"Irza");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
